a2d691ad52114f6e72c8427d10b40f388bd6dfc8
최철 <zxc8289@naver.com>
2025-10-02 13:38:03 +0900

    수정

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,26 @@
-// next.config.ts
-
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'lostark_front';
+
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+
+
+  basePath: isProd ? `/${repo}` : undefined,
+  assetPrefix: isProd ? `/${repo}/` : undefined,
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.discordapp.com',
-        port: '',
         pathname: '/avatars/**',
       },
     ],
   },
 };
 
-export default {
-  output: 'export',          // 중요: 정적 export
-  images: { unoptimized: true }, // GitHub Pages에서 이미지 최적화 비활성화
-  trailingSlash: true,       // 폴더형 index.html 생성 -> 딥링크 404 방지에 유리
-  // 사용자/조직 페이지가 아니라 "프로젝트 페이지"라면 아래 두 줄을 켜세요:
-  // basePath: isProd ? `/${repo}` : '',
-  // assetPrefix: isProd ? `/${repo}/` : '',
-}
+export default nextConfig;
